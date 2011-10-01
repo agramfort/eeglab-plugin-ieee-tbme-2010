09fe94aefec1661ca76d9d60bdacc098effd1214
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2011-10-01 15:37:58 -0400

    setup gh pages

--- lagextraction/eegplugin_lagextract.m
+++
@@ -1,5 +0,0 @@
-function eegplugin_extractlag( fig, try_strings, catch_strings)
-
-
-uimenu( findobj(fig, 'tag', 'tools') , 'label', 'Extract lags', 'callback', ... 
-           [try_strings.no_check '[EEG LASTCOM]= pop_extractlag(EEG);' catch_strings.store_and_hist '[ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG, CURRENTSET);' ]);
--- lagextraction/private/compute_weights.m
+++
@@ -1,6 +0,0 @@
-function weights = compute_weights(distances,sigma)
-% compute the weights matrix from the distance matrix
-% with a gaussian kernel
-
-
-weights = exp(-distances.^2/sigma^2);
